be51e23b1df94ef929b7293a9f76ef998a6cce32
charankumar07 <charankumar0999@gmail.com>
2025-02-26 17:14:54 +0530

    practicing

--- vars/nodejsVM.groovy
+++ vars/nodejsVM.groovy
@@ -1,5 +1,3 @@
-//oka centralised pipeline rastunam , e pipeline dheniki work avuthadi any nodejs project which is going to be deployed into the VM.
-//nodejsVM.groovy (nodejs programming language ki VM lo deploy chese application ki e pipeline annedi work avuthundhi)
 def call(Map configMap){
     pipeline {
         agent {
@@ -8,18 +6,18 @@ def call(Map configMap){
             }
         }
         environment { 
-            packageVesion = ''
-            //can maintain in pipeline globals
-            //nexusURL = '172.31.37.164:8081'
+            packageVersion = ''
+            // can maintain in pipeline globals
+            //nexusURL = '172.31.5.95:8081'
         }
         options {
             timeout(time: 1, unit: 'HOURS')
             disableConcurrentBuilds()
         }
         parameters {
-        //     string(name: 'PERSON', defaultValue: 'Mr Jenkins', description: 'Who should I say hello to?')
+            // string(name: 'PERSON', defaultValue: 'Mr Jenkins', description: 'Who should I say hello to?')
 
-        //     text(name: 'BIOGRAPHY', defaultValue: '', description: 'Enter some information about the person')
+            // text(name: 'BIOGRAPHY', defaultValue: '', description: 'Enter some information about the person')
 
             booleanParam(name: 'Deploy', defaultValue: false, description: 'Toggle this value')
 
@@ -29,95 +27,94 @@ def call(Map configMap){
         }
         // build
         stages {
-        stage('Get the version') {
-            steps {
-                script{
-                    def packageJson = readJSON file: 'package.json'
-                    packageVesion = packageJson.version
-                    echo "application version is $packageVesion"
+            stage('Get the version') {
+                steps {
+                    script {
+                        def packageJson = readJSON file: 'package.json'
+                        packageVersion = packageJson.version
+                        echo "application version: $packageVersion"
+                    }
                 }
             }
-        }
-        stage('Install dependencies') {
-            steps {
-                sh """
-                    npm install
-                """
+            stage('Install dependencies') {
+                steps {
+                    sh """
+                        npm install
+                    """
+                }
             }
-        }
-
-        stage('Unit tests') {
-            steps {
-                sh """
-                    echo "unit tests will run here "
-                """
+            stage('Unit tests') {
+                steps {
+                    sh """
+                        echo "unit tests will run here"
+                    """
+                }
             }
-        }
-
-        stage('Sonar Scan'){
-            steps{
-                sh """
-                    sonar-scanner
-                """
+            stage('Sonar Scan'){
+                steps{
+                    sh """
+                        echo "usually command here is sonar-scanner"
+                        echo "sonar scan will run here"
+                    """
+                }
             }
-        }
-        stage('Build') {
-            steps {
-                sh """
-                    ls -la
-                    zip -q -r ${configMap.component}.zip ./* -x ".git" -x "*.zip"
-                    ls -ltr
-                """
+            stage('Build') {
+                steps {
+                    sh """
+                        ls -la
+                        zip -q -r ${configMap.component}.zip ./* -x ".git" -x "*.zip"
+                        ls -ltr
+                    """
+                }
             }
-        }
-        stage('Publish artifacts') {
-            steps {
-                nexusArtifactUploader(
-                    nexusVersion: 'nexus3',
-                    protocol: 'http',
-                    nexusUrl: pipelineGlobals.nexusURL(),
-                    groupId: 'com.roboshop',
-                    version: "${packageVesion}",
-                    repository: "${configMap.component}",
-                    credentialsId: 'nexus-auth',
-                    artifacts: [
-                        [artifactId: "${configMap.component}",
+            stage('Publish Artifact') {
+                steps {
+                    nexusArtifactUploader(
+                        nexusVersion: 'nexus3',
+                        protocol: 'http',
+                        nexusUrl: pipelineGlobals.nexusURL(),
+                        groupId: 'com.roboshop',
+                        version: "${packageVersion}",
+                        repository: "${configMap.component}",
+                        credentialsId: 'nexus-auth',
+                        artifacts: [
+                            [artifactId: "${configMap.component}",
                             classifier: '',
                             file: "${configMap.component}.zip",
                             type: 'zip']
-                    ]
-                )
-            }
-        }
-        stage('Deploy') {
-            when {
-                expression{
-                    params.Deploy
-                }
-            }
-            steps {
-                script {
-                        def params = [
-                            string(name: 'version', value: "$packageVersion"),
-                            string(name: 'environment', value: "dev")
-                            booleanParam(name: 'Create', value: "${params.Deploy}")
                         ]
-                        build job: "../${configMap.component}-deploy", wait: true, parameters: params
+                    )
+                }
+            }
+            stage('Deploy') {
+                when {
+                    expression{
+                        params.Deploy
+                    }
+                }
+                steps {
+                    script {
+                            def params = [
+                                string(name: 'version', value: "$packageVersion"),
+                                string(name: 'environment', value: "dev")
+                                booleanParam(name: 'Create', value: "${params.Deploy}")
+                            ]
+                            build job: "../${configMap.component}-deploy", wait: true, parameters: params
+                        }
                 }
             }
         }
-    }
-        // Post build means build ipoena tharuwatha em cheyali
+        // post build
         post { 
             always { 
                 echo 'I will always say Hello again!'
                 deleteDir()
             }
             failure { 
-                echo 'This runs when pipeline is failed , used generally to send some alerts !'
+                echo 'this runs when pipeline is failed, used generally to send some alerts'
             }
-            success { 
-                echo 'I will say Hello when pipeline is success !'
+            success{
+                echo 'I will say Hello when pipeline is success'
             }
         }
     }
